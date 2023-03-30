5a9d47758c2f29b673b80eb93bc19ed4c4d337ca
dongquoctien(tom) <tien.dq@ohmyhotel.com>
2023-03-30 13:32:51 +0700

    update controller

--- src/main/java/com/example/accessingdatamysql/controller/StudentsController.java
+++ src/main/java/com/example/accessingdatamysql/controller/StudentsController.java
@@ -3,10 +3,10 @@ package com.example.accessingdatamysql.controller;
 import com.example.accessingdatamysql.model.Students;
 import com.example.accessingdatamysql.repository.IStudentsRespository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StudentsController {
@@ -16,9 +16,51 @@ public class StudentsController {
 
     @GetMapping("/students")
     public List<Students> index(){
+
         return studentsRespository.findAll();
     }
 
+    //Get search request parameter
+    @GetMapping("/students-by")
+    public  List<Students> indexBy(String name, String code){
+        return studentsRespository.findStudentsByNameContainsAndEmailContains(name, code);
+    }
 
+    //Get detail by id
+    @GetMapping("/students/{id}")
+    public Students detail(@PathVariable String id){
+        int studentId = Integer.parseInt(id);
+        return studentsRespository.getReferenceById(studentId);
+    }
 
+    //Request body
+    @PostMapping("/students-search")
+    public List<Students> searchMutiple(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return studentsRespository.findStudentsByNameContainsAndEmailContains(searchTerm, searchTerm);
+    }
+
+    @PostMapping("/students-create")
+    public Students createStudent(@RequestBody Map<String, String> body){
+        String name = body.get("name");
+        Integer age =  Integer.parseInt( body.get("age") != null ? body.get("age") :"0");
+        String email = body.get("email");
+
+        return studentsRespository.save(new Students( name, age, email));
+    }
+
+    @PutMapping("/students-update/{id}")
+    public Students update(@PathVariable String id, @RequestBody Map<String, String> body){
+        int blogId = Integer.parseInt(id);
+        // get student
+        Students student = studentsRespository.getReferenceById(blogId);
+        String name = body.get("name");
+        Integer age =  Integer.parseInt( body.get("age") != null ? body.get("age") :"0");
+        String email = body.get("email");
+
+        student.setName(name);
+        student.setAge(age);
+        student.setEmail(email);
+        return studentsRespository.save(student);
+    }
 }
